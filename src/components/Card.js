9d2636e5df1e23f2707fe57208e9436590e45395
Harshit <helloharshit68@gmail.com>
2020-02-14 22:50:21 +0530

    tweek

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card= (props) => {
 	return (
 		<div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt="nigas" src={`https://robohash.org/${props.name}?125x125`}/>
+			<img alt="robos" src={`https://robohash.org/${props.name}?125x125`}/>
 			<div>
 				<h2>{props.name}</h2>
 				<p>{props.email}</p>
